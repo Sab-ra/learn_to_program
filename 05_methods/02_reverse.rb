7e610b595840509442bdef60f2cf76e7f5286085
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2024-01-16 23:39:12 +0200

    Start learning methods

---
+++ 05_methods/02_reverse.rb
@@ -0,0 +1,15 @@
+var1 = 'stop'
+var2 = 'deliver repaid desserts'
+var3 = '....TSELES B HSUP - A magic spell?'
+
+puts ''
+puts var1.reverse
+puts var2.reverse!
+puts var3.reverse
+
+puts ''
+
+puts var1
+puts var2
+puts var3
+puts ''
---
+++ 05_methods/03_length.rb
@@ -0,0 +1,7 @@
+puts ''
+print "Whats your full name? "
+name = gets.chomp
+puts ''
+puts "Did you know there are #{ name.length } characters in your name,"
+puts name + "?"
+puts ''
