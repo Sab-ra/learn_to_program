c64447df0779910b3d212d9c714ce1a34ac1209f
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2024-01-17 00:21:23 +0200

    Make table of contents

---
+++ 05_methods/06_table_of_contents.rb
@@ -0,0 +1,12 @@
+puts ''
+
+line_width = 60
+
+puts( "Table of Contents".center( line_width ))
+puts ''
+puts( "CHAPTER 1: Numbers".ljust( line_width / 4 * 3 ) + "page 3".rjust( line_width / 4 ))
+puts( "CHAPTER 2: Letters".ljust( line_width / 4 * 3 ) + "page 7".rjust( line_width / 4 ))
+puts( "CHAPTER 3: Variables".ljust( line_width / 4 * 3 ) + "page 15".rjust( line_width / 4 ))
+puts( "CHAPTER 4: What a fuck is going on".ljust( line_width / 4 * 3 ) + "page 321".rjust( line_width / 4 ))
+
+puts ''
