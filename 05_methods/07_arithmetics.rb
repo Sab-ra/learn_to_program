ad7504a269c4de50ad75c46013e0a00dafc36a8a
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2024-01-17 15:50:39 +0200

    Learn extra arithm methods

---
+++ 05_methods/07_arithmetics.rb
@@ -0,0 +1,20 @@
+# Exponentation
+puts ''
+puts 5**2
+puts ''
+puts 5**0.5
+
+# Modulus
+puts ''
+puts 7%3
+puts ''
+puts 365%7
+puts ''
+puts 366%7
+puts '' # modulus with floats ))
+puts 5%2.1
+
+# ABS
+puts''
+puts ( 5 - 2 ).abs 
+puts ( 2 - 5 ).abs
