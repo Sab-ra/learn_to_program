e949a19007a83e6dbfeb088d5aa79b69ba289829
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2024-01-17 17:04:16 +0200

    Learn Math object

---
+++ 05_methods/10_the_math_object.rb
@@ -0,0 +1,13 @@
+# The Math Object holds sine and cosine and something else
+puts ''
+puts( Math::PI )
+puts ''
+puts( Math::E )
+puts ''
+puts( Math.cos( Math::PI/3 ))
+puts ''
+puts( Math.tan( Math::PI/4 ))
+puts ''
+puts( Math.log( Math::E**2 ))
+puts ''
+puts( ( 1 + Math.sqrt( 5 )) / 2 ) # golden ratio
