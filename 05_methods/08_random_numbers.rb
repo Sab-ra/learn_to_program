2e4078c94526511ba030f2ca723e29a9fcb73935
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2024-01-17 16:19:32 +0200

    Deal random numbers

---
+++ 05_methods/08_random_numbers.rb
@@ -0,0 +1,27 @@
+# Random 0.0 >= rand < 1.0
+puts ''
+puts rand 
+puts rand
+puts rand
+
+# Random with limits 0 >= rand < 5
+puts ''
+puts( rand( 5 ))
+puts( rand( 5 ))
+puts( rand( 5 ))
+
+# More random examples
+puts ''
+puts( rand( 100 ))
+puts( rand( 100 ))
+puts( rand( 100 ))
+puts ''
+puts( rand( 1 ))
+puts( rand( 1 ))
+puts( rand( 1 ))
+puts''
+puts( rand( 99999999999999999999999999999999999 ))
+puts''
+puts( "My weather app said there is a" )
+puts( "#{rand( 101 )}% chance of rain," )
+puts( "but it's literally raining right now!" )
---
+++ 05_methods/09_strand_random.rb
@@ -0,0 +1,54 @@
+# Setting the seed with srand method
+# srand stands for seed rand
+puts ''
+srand 1976
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts ''
+srand 1976
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts ''
+# Change srand value
+srand 19768
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts ''
+srand 19768
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts ''
+# It seems theat's what they are doing with playlists
+# When you start mixing play, it repeats the sequence
+
+# Can you make srund value also random every time?
+
+# Last experiment, I'll not give any value to srand
+srand 
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts ''
+srand 
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+puts( rand( 100))
+
+# Oh! That how it works!!! This answers my previous
+# (line 36) question!!!!!!!
