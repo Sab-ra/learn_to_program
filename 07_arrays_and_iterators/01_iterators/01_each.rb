f9bb2b010551041f50ff545a16015f50bf535562
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2024-01-24 22:59:06 +0200

    Learn each for arrays

---
+++ 07_arrays_and_iterators/01_iterators/01_each.rb
@@ -0,0 +1,12 @@
+langs = [ "English", "Norwegian", "Ruby" ]
+
+langs.each do | lang |
+  puts
+  puts "I love #{ lang }! Don't you?"
+end
+
+puts
+
+puts "And let's hear it for Fortran!"
+puts "<crickets chirp in the distance>"
+puts 
