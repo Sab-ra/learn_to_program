fa7a7b4125125eaf48243d800008109b62fb5736
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2024-02-10 14:42:28 +0200

    Understand diff class-object

---
+++ 11_classes_and_methods/01_new.rb
@@ -0,0 +1,9 @@
+alpha = Array.new + [ 12345, "what a fuck!" ]
+beta = String.new + "hello"
+karma = Time.new
+
+puts "alpha = #{ alpha }"
+puts "beta = #{ beta }"
+puts "karma = #{ karma }"
+
+puts alpha[ 1 ]
