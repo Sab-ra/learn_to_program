e083191fffa488143b7ba8c10b891b483cc1dff9
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2024-02-13 11:04:24 +0200

    Dive deeper in hashes

--- 11_classes_and_methods/05_hash_eu_capitals.rb
+++ 11_classes_and_methods/05_hash_eu_capitals.rb
@@ -20,3 +20,38 @@ end
 captials_hash.each do | country, capital |
   puts "#{ country } - #{ capital }"
 end
+
+puts # butch literal loading of array VS hash
+
+greetings = [ "hello", "hey", "what's up!" ]
+smoothes = { "mongo" => "yum", "garlic" => "yuck" }
+
+puts greetings
+puts smoothes
+puts smoothes[ "mongo" ]
+puts smoothes[ "yuck" ] # this puts just an empty string.
+
+puts # You can any object as a hash key.
+
+weird_hash = Hash.new
+
+weird_hash[ 12 ] = "monkeys"
+weird_hash[ [] ] = "void"
+weird_hash[ Time.new + 5 ] = "no time like the present"
+
+weird_hash.each do | key, value |
+  puts "#{ key }: #{ value }"
+end
+
+puts # arrays for similar things, hashes for different
+
+myself = { "name" => "Keira", "pairs_of_shoes" => 5 }
+imelda = { "name" => "Imelda", "pairs_of_shoes" => 7242 }
+
+people = [ myself, imelda ]
+
+people.each do | reveal |
+  reveal.each do | name, has |
+    puts "#{ name } - #{ has }"
+  end
+end
