7fcc4acf3b1133470c3f24222da103242105fa75
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2024-02-16 10:01:29 +0200

    Understand substrings

---
+++ 11_classes_and_methods/07_stringy_superpow.rb
@@ -0,0 +1,22 @@
+# Array methods to strings
+
+bosco = "Mr. T"
+big_T = bosco[ 4 ]
+
+puts big_T
+
+puts
+
+puts "Hello. My name is Apollo."
+puts "I'm extremelly perspective."
+puts "What's your name?"
+
+name = gets.chomp
+
+puts
+puts "Hi #{ name }"
+
+if name[ 0 ] == "C"
+  puts "You have excelent taste in footwear."
+  puts "I can just tell."
+end
